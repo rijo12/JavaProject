bc8985eb875518d7f6547c7d5f8bb5f8d0ed9e03
rijo12 <pcrijo@gmail.com>
2022-05-28 17:10:44 -0400

    Console Input

--- src/assignment3/ShopCount.java
+++ src/assignment3/ShopCount.java
@@ -1,24 +1,55 @@
 package assignment3;
 
+import java.util.Scanner;
+
 public class ShopCount {
 
 	public static void main(String[] args) {
-		int maximumCapacity = 100;
+		int restrictedCount = 50;
+		int currentCount = 20;// People inside the shop assumed to be zero.
+		int peopleToEnter=0;
 		
-		int restrictedCapacity = 50;
+		System.out.println("Number of people to enter the shop");
 		
-		int currentCapacity = 48;
+		Scanner sc= new Scanner(System.in);
+		peopleToEnter = sc.nextInt();
 		
-		while (currentCapacity < maximumCapacity) {
+		
+			if(peopleToEnter==0) {
+				System.out.println("People in the shop"+ currentCount); // checks if the people wants to enter the shop is zero
 			
-			currentCapacity++;
-			
-			if (currentCapacity == 50) {
-				
-				break;
 			}
+			
+			if (currentCount< restrictedCount) { 
+			
+				while(peopleToEnter<50 ) {
+					
+					currentCount+=peopleToEnter;
+					
+					if(currentCount==50) {                                     // checks if the current count is 50
+						
+						System.out.println("People count inside the shop=50");
+						System.out.println("Maximum Capacity Reached");
+						break;
+						
+						}
+					
+					if (currentCount>=50) {
+						
+						System.out.println("people wait outside="+(currentCount-restrictedCount));
+						System.out.println("People count inside the shop=50");
+						System.out.println("Maximum Capacity Reached");
+						break;
+						
+						}
+					
+					System.out.println("People count inside the shop="+currentCount);
+			
+			System.out.println("people can enter=" + (restrictedCount- currentCount));
 		}
-		System.out.println("Maximum Capacity reached");
+			
+		}	
+			
 	}
 
 }
